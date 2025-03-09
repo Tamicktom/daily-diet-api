c448bb53bb3304c959fe79b49c869f158b01435b
Tamicktom <rickpokemon38@gmail.com>
2025-03-08 22:13:39 -0300

    add user and meals tables with relationships in schema

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,9 +1,36 @@
 //* Libraries imports
-import { int, sqliteTable, text } from "drizzle-orm/sqlite-core";
+import { sql, relations } from "drizzle-orm";
+import { int, integer, sqliteTable, text } from "drizzle-orm/sqlite-core";
 
 export const usersTable = sqliteTable("users", {
   id: int().primaryKey({ autoIncrement: true }),
   name: text().notNull(),
   age: int().notNull(),
   email: text().notNull().unique(),
+  password: text().notNull(),
+  created_at: text().default(sql`(CURRENT_TIMESTAMP)`),
+  updated_at: text().default(sql`(CURRENT_TIMESTAMP)`),
 });
+
+export const usersMealsRelation = relations(usersTable, ({ many }) => ({
+  meals: many(mealsTable),
+}));
+
+export const mealsTable = sqliteTable("meals", {
+  id: int().primaryKey({ autoIncrement: true }),
+  name: text(),
+  description: text(),
+  date_time: text().notNull(),
+  is_on_diet: integer({ mode: "boolean" }).notNull(),
+  created_at: text().default(sql`(CURRENT_TIMESTAMP)`),
+  updated_at: text().default(sql`(CURRENT_TIMESTAMP)`),
+
+  user_id: int().notNull(),
+});
+
+export const mealsUserRelation = relations(mealsTable, ({ one }) => ({
+  user: one(usersTable, {
+    fields: [mealsTable.user_id],
+    references: [usersTable.id]
+  }),
+}));
